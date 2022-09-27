511a29606c0cd101d1eee0eccf01876136ab2541
Alexandre Dufresne Nappert <93613553+MrDNAlex@users.noreply.github.com>
2022-09-26 22:20:37 -0400

    New Update
    Added that the user can click the button in the list to add the word manually
    Replaced the Word list with the official Wordle Word List

--- lib/WordClass.dart
+++ lib/WordClass.dart
@@ -62,7 +62,7 @@ class WordClass {
 
   List<String> calcPossibleWords() {
     // List<String> listCopy = allWords;
-    List<String> wordList = allWords3;
+    List<String> wordList = allWords;
     // List<String> wordList2 = allWords;
 
     // print(removeLetters);
@@ -378,7 +378,7 @@ class WordClass {
   }
 
   static bool wordInList(String word, bool upper) {
-    List<String> wordList = allWords3;
+    List<String> wordList = allWords;
 
     List<String> newWords = [];
     for (String i in wordList) {
